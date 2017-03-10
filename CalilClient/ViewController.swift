4c1753b2b2e7e40dcefc12a6edd6f91139ba8b0b
yu kawase <kawase@aloe-project.com>
2017-03-10 20:09:23 +0900

    Initial Commit

---
+++ CalilClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalilClient
+//
+//  Created by kawase yu on 2017/03/10.
+//  Copyright © 2017年 u kawse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
